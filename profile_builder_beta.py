991923767aa861e66afd7adf1588d18c67c1e7d2
jwcomplete <68453307+jwcomplete@users.noreply.github.com>
2025-03-27 03:02:13 -0400

    Update profile_builder_beta.py

--- profile_builder_beta.py
+++ profile_builder_beta.py
@@ -19,7 +19,7 @@ st.markdown("""
     .stToggle {
         display: flex;
         align-items: flex-start !important;
-        margin-top: -0.5rem;
+        margin-top: -0.25rem;
     }
 </style>
 """, unsafe_allow_html=True)
