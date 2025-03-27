a3e099cab2d7671cf4a1b294036e403f393b9ad2
jwcomplete <68453307+jwcomplete@users.noreply.github.com>
2025-03-27 02:45:43 -0400

    Update profile_builder_beta.py

--- profile_builder_beta.py
+++ profile_builder_beta.py
@@ -48,17 +48,17 @@ with col1:
         accessory_unit = st.selectbox(" ", ["No", "Yes"], key="accessory_unit")
 
     if accessory_unit == "Yes":
-        col_sep_q, col_sep_a = st.columns([3, 1])
+        col_sep_q, col_sep_a = st.columns([3, 3])
         with col_sep_q:
             st.markdown("**Interior separation from primary unit?**")
         with col_sep_a:
             interior_separation = st.selectbox(" ", ["No", "Yes"], key="interior_separation")
-        col_kitch_q, col_kitch_a = st.columns([3, 1])
+        col_kitch_q, col_kitch_a = st.columns([3, 3])
         with col_kitch_q:
             st.markdown("**Kitchen with fridge, stove, sink?**")
         with col_kitch_a:
             has_kitchen = st.selectbox(" ", ["No", "Yes"], key="has_kitchen")
-        col_bath_q, col_bath_a = st.columns([3, 1])
+        col_bath_q, col_bath_a = st.columns([3, 3])
         with col_bath_q:
             st.markdown("**Bathroom present?**")
         with col_bath_a:
